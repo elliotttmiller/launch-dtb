12d7d812f5e3cc299204525769537cf7150cf492
Elliott Miller <elliotttmiller@hotmail.com>
2026-07-26 05:27:09 -0500

    Load Stripe express address integrity boundary

--- drywalltoolbox/wp/wp-content/mu-plugins/dtb-commerce/bootstrap.php
+++ drywalltoolbox/wp/wp-content/mu-plugins/dtb-commerce/bootstrap.php
@@ -23,6 +23,7 @@ require_once __DIR__ . '/Domain/PaymentState.php';
 require_once __DIR__ . '/Payment/WooNativeCheckoutRuntime.php';
 require_once __DIR__ . '/Payment/StorefrontReturnContext.php';
 require_once __DIR__ . '/Payment/OfficialStripeNativeCheckout.php';
+require_once __DIR__ . '/Payment/ExpressCheckoutAddressIntegrity.php';
 require_once __DIR__ . '/Payment/CheckoutPerformance.php';
 require_once __DIR__ . '/Payment/CheckoutRuntimeIntegrity.php';
 require_once __DIR__ . '/Shipping/DTBShippingMethod.php';
