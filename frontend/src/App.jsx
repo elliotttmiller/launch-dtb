13c35f5b214245bff3473f5440b6d98568443714
Elliott Miller <elliotttmiller@hotmail.com>
2026-08-22 15:26:53 -0500

    Route repairs landing to dedicated production page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -78,7 +78,7 @@ const Product = lazyWithReload(() => import('./pages/Product'));
 const ProductDetailPage = lazyWithReload(() => import('./pages/ProductDetailPage'));
 const CategoryLandingPage = lazyWithReload(() => import('./pages/CategoryLandingPage'));
 const Schematics = lazyWithReload(() => import('./pages/SchematicsPage'));
-const Repairs = lazyWithReload(() => import('./pages/Repairs'));
+const Repairs = lazyWithReload(() => import('./pages/RepairLanding'));
 const RepairStart = lazyWithReload(() => import('./pages/RepairStart'));
 const RepairPackages = lazyWithReload(() => import('./pages/RepairPackages'));
 const RepairTrack = lazyWithReload(() => import('./pages/RepairTrack'));
