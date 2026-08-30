63ec577c4f7eecf923db71156173e670a789d416
Elliott Miller <elliotttmiller@hotmail.com>
2026-08-30 02:12:42 -0500

    Fix checkout header logo asset path

--- drywalltoolbox/wp/wp-content/themes/drywall-toolbox/templates/checkout/native-checkout.php
+++ drywalltoolbox/wp/wp-content/themes/drywall-toolbox/templates/checkout/native-checkout.php
@@ -49,7 +49,7 @@ wp_enqueue_style(
 	<header class="dtb-checkout__topbar">
 		<div class="dtb-checkout__topbar-inner">
 			<a class="dtb-checkout__brand" href="<?php echo esc_url( $storefront_home_url ); ?>">
-				<img src="<?php echo esc_url( home_url( '/logo-white.svg' ) ); ?>" alt="<?php esc_attr_e( 'Drywall Toolbox', 'drywall-toolbox' ); ?>" width="3000" height="917">
+				<img src="<?php echo esc_url( home_url( '/logos/logo-white.svg' ) ); ?>" alt="<?php esc_attr_e( 'Drywall Toolbox', 'drywall-toolbox' ); ?>" width="3000" height="917">
 			</a>
 			<span class="dtb-checkout__stripe-badge">
 				<svg viewBox="0 0 16 16" aria-hidden="true" focusable="false"><path fill="currentColor" d="M8 0a3 3 0 0 0-3 3v2H4a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1h-1V3a3 3 0 0 0-3-3Zm0 1.5A1.5 1.5 0 0 1 9.5 3v2h-3V3A1.5 1.5 0 0 1 8 1.5Z"/></svg>
