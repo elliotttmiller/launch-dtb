f3fdcc3ca13b4c788c07c13910727db1f9833d6c
unknown <elliotttmiller@hotmail.com>
2026-08-30 21:23:20 -0500

    Update - Drywalltoolbox

--- frontend/src/assets/payment-methods/index.js
+++ frontend/src/assets/payment-methods/index.js
@@ -1,9 +1,30 @@
 import affirmLogo from './affirm.svg';
 import afterpayLogo from './afterpay.svg';
+import americanExpressLogo from './american-express.svg';
 import applePayLogo from './apple-pay.svg';
 import googlePayLogo from './google-pay.svg';
 import klarnaLogo from './klarna.svg';
+import mastercardLogo from './mastercard.svg';
 import paypalLogo from './paypal.svg';
+import visaLogo from './visa.svg';
+
+export const PAYMENT_CARD_NETWORK_ASSETS = Object.freeze({
+  americanExpress: Object.freeze({
+    id: 'american-express',
+    label: 'American Express',
+    src: americanExpressLogo,
+  }),
+  mastercard: Object.freeze({
+    id: 'mastercard',
+    label: 'Mastercard',
+    src: mastercardLogo,
+  }),
+  visa: Object.freeze({
+    id: 'visa',
+    label: 'Visa',
+    src: visaLogo,
+  }),
+});
 
 export const EXPRESS_CHECKOUT_METHODS = Object.freeze([
   {
